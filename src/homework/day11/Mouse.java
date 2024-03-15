331b7f37c203ffec73b5fe73452c68a0ca18227c
Alexej Bond <alexej.bondarik@gmail.com>
2024-03-15 22:39:57 +0100

    Day 11, Tasks 1-4 complete

--- src/homework/day11/Mouse.java
+++ src/homework/day11/Mouse.java
@@ -1,2 +1,28 @@
-package homework.day11;public class Mouse {
+package homework.day11;
+
+public class Mouse {
+
+    private String pattern = "Mouse <n>";
+    private String name;
+
+    public Mouse(int number) {
+        name = pattern.replace("<n>", String.valueOf(number));
+    }
+
+    public void peep() {
+        System.out.println(name + ": PEEP!");
+        try {
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+//    public static void main(String[] args) {
+//        Mouse mouse = new Mouse(1);
+//        mouse.peep();
+//    }
+
 }
--- src/homework/day11/Thread_2.java
+++ src/homework/day11/Thread_2.java
@@ -1,2 +1,24 @@
-package homework.day11;public class Thread_2 {
+package homework.day11;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Thread_2 {
+    public static void main(String[] args) {
+
+
+        for (int i = 0; i < 7; i++) {
+            new Thread(() -> {
+                List<Mouse> miceList = new ArrayList<>();
+                for (int m = 0; m < 18; m++) {
+                    miceList.add(new Mouse(m));
+                }
+                for (int n = 0; n < miceList.size(); n++) {
+                    if (n % 2 == 0)
+                        miceList.get(n).peep();
+                }
+            }).start();
+        }
+    }
 }
+
